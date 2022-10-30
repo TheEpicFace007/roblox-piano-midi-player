09bb431bf3cc3525a856dee512e05f82c63ab4e7
Charlie <37189791+TheEpicFace007@users.noreply.github.com>
2022-10-29 20:04:16 -0400

    add script

---
+++ piano-player.lua
@@ -0,0 +1,3 @@
+local midiParser = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheEpicFace007/roblox-piano-midi-player/master/midi-lib.lua"), "midi-lib.lua")()
+
+local HesaPirate = midiParser("HesaPirate.mid")
